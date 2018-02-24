970c4dbfcd246e5137431b4b0c13b977f482659d
cgy <guangyuanchen001@163.com>
2018-02-24 15:15:49 +0800

    knet/detail/AsyncReceivedMsgCB.cpp

--- knet/detail/AsyncReceivedMsgCB.cpp
+++ knet/detail/AsyncReceivedMsgCB.cpp
@@ -6,26 +6,7 @@ namespace knet {
 
 namespace detail {
 
-AsyncReceivedMsgCB::AsyncReceivedMsgCB(RPCManager& rpc)
-    : m_rpc(rpc)
-{
 
-}
-    
-AsyncReceivedMsgCB::~AsyncReceivedMsgCB()
-{
-
-}
-
-void AsyncReceivedMsgCB::onError(int32_t err_no, const std::string& err_str)
-{
-    ReceiveMessageCB::onError(err_no, err_str);
-}
-
-void AsyncReceivedMsgCB::onReceived(const void* p, size_t p_len, const void* key, size_t key_len)
-{
-    m_rpc.CB_ReceviedMsg(p, p_len, key, key_len);
-}
 
 } // detail
 
